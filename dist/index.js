93d31d76f7885c5e6316fcc16620b31efec97bd7
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2020-09-06 19:30:00 +0200

    v0.0.1-alpha.2

--- dist/index.js
+++ dist/index.js
@@ -102,6 +102,25 @@ exports.setGithubActionOutputFromResults = setGithubActionOutputFromResults;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -115,10 +134,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__webpack_require__(2186));
 const main_1 = __importDefault(__webpack_require__(3109));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield main_1.default();
+        try {
+            yield main_1.default();
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
     });
 }
 run();
